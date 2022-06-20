e9e07a25052133c531812ed9f1e7b0457e5a2af5
Henrik Larsson <henke@henkelarsson.se>
2022-06-20 08:35:49 +0200

    Add calculations in atr

--- shared/src/main/java/org/trading/event/Atr.java
+++ shared/src/main/java/org/trading/event/Atr.java
@@ -5,12 +5,34 @@ import lombok.Data;
 import org.kie.api.definition.type.Expires;
 import org.kie.api.definition.type.Role;
 import org.kie.api.definition.type.Role.Type;
+import org.trading.SystemProperties;
+import org.trading.model.MarketInfo;
 
 @AllArgsConstructor
 @Data
 @Role(Type.EVENT)
 @Expires("5s")
-public class Atr {
+public class Atr extends SystemProperties {
   String epic;
   Double level;
+
+  // TODO some market can take partial size like 1.5 contracts
+  public Double positionSize(MarketInfo marketInfo, AccountEquity accountEquity) {
+    var equityRiskPerTrade = percentageRiskPerOrder * accountEquity.getEquity();
+    var riskPerUnit = level * oneRMultipleOfAtr * marketInfo.getValueOfOnePip();
+    var numberContract = Math.floor(equityRiskPerTrade / riskPerUnit); // Round down
+    // Make sure number of contract are larger then the minimum for the market
+    if (marketInfo.getLotSize() > numberContract) {
+      return 0.;
+    }
+    return numberContract;
+  }
+
+  public Double targetDistance() {
+    return riskRewardRation * stopDistance();
+  }
+
+  public Double stopDistance() {
+    return oneRMultipleOfAtr * level;
+  }
 }
--- shared/src/main/java/org/trading/model/MarketInfo.java
+++ shared/src/main/java/org/trading/model/MarketInfo.java
@@ -9,17 +9,18 @@ import lombok.Data;
 // TODO add validation to this class so when i read props it get validated
 @Data
 public class MarketInfo {
-  String epic;
-  String expiry;
-  String currency;
-  Double minStop;
-  Double maxStopMultiplier;
-  Double lotSize;
-  LocalTime localOpenTime;
-  LocalTime localCloseTime;
-  String marketZone;
-  Set<LocalDate> nonTradingDays;
-  Long barsInOpeningRange;
+  private String epic;
+  private String expiry;
+  private String currency;
+  private Double minStop;
+  private Double maxStopMultiplier;
+  private Double lotSize;
+  private LocalTime localOpenTime;
+  private LocalTime localCloseTime;
+  private String marketZone;
+  private Set<LocalDate> nonTradingDays;
+  private Long barsInOpeningRange;
+  private Double valueOfOnePip;
 
   public boolean isEu() {
     return marketZone.equals("EU");
