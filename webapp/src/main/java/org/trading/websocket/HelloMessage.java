9a99eeb28fc1ccde0e40990a2c1d1c34c2bfde81
Henrik Larsson <henke@henkelarsson.se>
2022-06-22 19:30:28 +0200

    Ading Greeting websocket STOP example

---
+++ webapp/src/main/java/org/trading/websocket/HelloMessage.java
@@ -0,0 +1,13 @@
+package org.trading.websocket;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+  private String name;
+}
